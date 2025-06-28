9c87fb996f57f95c5cb3f4e8dc1b5045aedf24f9
“farah-jannat” <farahjannatmithila@gmail.com>
2025-06-28 17:00:49 +0600

    feat: add menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const menuItems = [
   {
     title: "MENU",
@@ -113,9 +116,27 @@ const menuItems = [
   },
 ];
 
-const Menu = ()=>{
-  return(
-    <div></div>
-  )
-}
+const Menu = () => {
+  return (
+    <div>
+      {menuItems.map((menus) => (
+        <div>
+          <span className="hidden lg:block text-gray-400 font-light my-4">
+            {menus.title}
+          </span>
+          {menus.items.map((item) => (
+            <Link
+              href={item.href}
+              key={item.label}
+              className="flex items-center justify-center lg:justify-start gap-4  text-gray-500 py-2 md:px-2 rounded-md hover:bg-blue-100"
+            >
+              <Image src={item.icon} alt="" width={20} height={20} />
+              <span className="hidden lg:block">{item.label}</span>
+            </Link>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
 export default Menu;
