ec5dd34f67baba4afab89634c15d2bfb6009e728
“farah-jannat” <farahjannatmithila@gmail.com>
2025-06-30 10:25:22 +0600

    feat: add menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -128,7 +128,7 @@ const Menu = () => {
             <Link
               href={item.href}
               key={item.label}
-              className="flex items-center justify-center lg:justify-start gap-4  text-gray-500 py-2 md:px-2 rounded-md hover:bg-blue-100"
+              className="flex items-center justify-center lg:justify-start gap-4  text-gray-500 py-2 md:px-2 rounded-md hover:bg-purple-100"
             >
               <Image src={item.icon} alt="" width={20} height={20} />
               <span className="hidden lg:block">{item.label}</span>
